c96e207b7901df5a9cbe8b97af3fee7554e30e8b
grondag <grondag@gmail.com>
2019-06-26 21:34:58 -0700

    Revert "Put some code in workaround to prevent being dropped?"
    This reverts commit 902b1fe21baf66fbf5dcbce4b477f31240ad2696.

--- src/main/java/grondag/frex/mixin/MixinHack.java
+++ src/main/java/grondag/frex/mixin/MixinHack.java
@@ -5,20 +5,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import grondag.frex.Frex;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedModelManager;
 
 @Mixin(BakedModelManager.class)
 public class MixinHack {
-    private static boolean isHackNeeded = true;
     
     /** sole purpose is to force BakedModel into the ref map */
     @Inject(at = @At("HEAD"), method = "getMissingModel")
     public void frex_egregiousHack(CallbackInfoReturnable<BakedModel> ci) {
-        if(isHackNeeded) {
-            Frex.LOG.debug("FREX empty mixin refmap workaround activated. This is normal");
-            isHackNeeded = false;
-        }
+        //NOOP
     }
 }
