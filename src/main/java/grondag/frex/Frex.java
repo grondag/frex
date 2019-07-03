a1b70268bcbdb827b10d80dd8957990d415c76f3
grondag <grondag@gmail.com>
2019-07-03 08:37:07 -0700

    Correctly report availability

--- src/main/java/grondag/frex/Frex.java
+++ src/main/java/grondag/frex/Frex.java
@@ -20,19 +20,28 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apiguardian.api.API;
 
-import net.fabricmc.fabric.api.renderer.v1.Renderer;
-import net.fabricmc.fabric.api.renderer.v1.RendererAccess;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class Frex {
     public static Logger LOG = LogManager.getLogger("FREX");
     
     private static int checkVal = -1;
     
+    // UGLY: use mod.json custom properties or something else less awful
     @API(status = API.Status.STABLE)
     public static boolean isAvailable() {
         if(checkVal == -1) {
-            Renderer renderer = RendererAccess.INSTANCE.getRenderer();
-            checkVal = renderer != null && renderer instanceof grondag.frex.api.Renderer ? 1 : 0;
+            // can't rely on renderer being available yet
+//            Renderer renderer = RendererAccess.INSTANCE.getRenderer();
+//            checkVal = renderer != null && renderer instanceof grondag.frex.api.Renderer ? 1 : 0;
+            FabricLoader.getInstance().getAllMods().forEach(m -> {
+                if(m.getMetadata().getId().equals("canvas")) {
+                    checkVal = 1;
+                }
+            });
+            if(checkVal == -1) {
+                checkVal = 0;
+            }
         }
         return checkVal == 1;
     }
