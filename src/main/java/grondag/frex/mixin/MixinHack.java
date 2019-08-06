b2744865d49cfbb4a25280417954c75c64bf7ced
grondag <grondag@gmail.com>
2019-08-06 07:21:36 -0700

    Try to force refmap generation

---
+++ src/main/java/grondag/frex/mixin/MixinHack.java
@@ -0,0 +1,17 @@
+package grondag.frex.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.render.model.BakedModel;
+
+@Mixin(BakedModel.class)
+public class MixinHack {
+    /** sole purpose is to force BakedModel into the ref map */
+    @Inject(at = @At("HEAD"), method = "getMissingModel")
+    public void frex_egregiousHack(CallbackInfoReturnable<BakedModel> ci) {
+        //NOOP
+    }
+}
