244d0d5f80c9ecc1c29ded77ffd6c56ab5f234f3
grondag <grondag@gmail.com>
2019-08-06 08:06:34 -0700

    More performant hack

--- src/main/java/grondag/frex/mixin/MixinHack.java
+++ src/main/java/grondag/frex/mixin/MixinHack.java
@@ -3,16 +3,16 @@ package grondag.frex.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.client.render.block.BlockModelRenderer;
 import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.BasicBakedModel;
 
-@Mixin(BlockModelRenderer.class)
+@Mixin(BasicBakedModel.Builder.class)
 public abstract class MixinHack {
     /** sole purpose is to force BakedModel into the ref map */
-    @Inject(at = @At("HEAD"), method = "render")
-    private void egregiousRefmapHack(BakedModel bakedModel_1, float float_1, float float_2, float float_3, float float_4, CallbackInfo ci) {
+    @Inject(at = @At("RETURN"), method = "build")
+    private void egregiousRefmapHack (CallbackInfoReturnable<BakedModel> ci) {
         //NOOP
      }
 }
