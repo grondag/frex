5055b7e0b5281b2083fe39dcc033078dd1abfadc
Grondag <grondag@gmail.com>
2019-11-21 18:50:32 -0800

    Format

--- src/main/java/grondag/frex/impl/material/MaterialLoaderImpl.java
+++ src/main/java/grondag/frex/impl/material/MaterialLoaderImpl.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2019 grondag
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -30,32 +30,32 @@ import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 
 public final class MaterialLoaderImpl {
-private MaterialLoaderImpl() {}
-    private static final ObjectOpenHashSet<Identifier> CAUGHT = new ObjectOpenHashSet<>();
-    
-    public static synchronized RenderMaterial loadMaterial(Identifier idIn) {
-        Renderer r = RendererAccess.INSTANCE.getRenderer();
-        RenderMaterial result = r.materialById(idIn);
-        if(result == null) {
-            result = loadMaterialInner(idIn);
-            r.registerMaterial(idIn, result);
-        }
-        return result;
-    }
-    
-    private static RenderMaterial loadMaterialInner(Identifier idIn) {
-        Identifier id = new Identifier(idIn.getNamespace(), "materials/" + idIn.getPath() + ".json");
-        
-        RenderMaterial result = null;
-        ResourceManager rm = MinecraftClient.getInstance().getResourceManager();
-        try(Resource res = rm.getResource(id)) {
-            result = MaterialDeserializer.deserialize(new InputStreamReader(res.getInputStream(), StandardCharsets.UTF_8));
-        } catch (Exception e) {
-            if(CAUGHT.add(idIn)) {
-                Frex.LOG.info("Unable to load render material " + idIn.toString() + " due to exception " + e.toString()); 
-            }
-        }
-        
-        return result;
-    }
+	private MaterialLoaderImpl() {}
+	private static final ObjectOpenHashSet<Identifier> CAUGHT = new ObjectOpenHashSet<>();
+
+	public static synchronized RenderMaterial loadMaterial(Identifier idIn) {
+		final Renderer r = RendererAccess.INSTANCE.getRenderer();
+		RenderMaterial result = r.materialById(idIn);
+		if(result == null) {
+			result = loadMaterialInner(idIn);
+			r.registerMaterial(idIn, result);
+		}
+		return result;
+	}
+
+	private static RenderMaterial loadMaterialInner(Identifier idIn) {
+		final Identifier id = new Identifier(idIn.getNamespace(), "materials/" + idIn.getPath() + ".json");
+		RenderMaterial result = null;
+		final ResourceManager rm = MinecraftClient.getInstance().getResourceManager();
+
+		try(Resource res = rm.getResource(id)) {
+			result = MaterialDeserializer.deserialize(new InputStreamReader(res.getInputStream(), StandardCharsets.UTF_8));
+		} catch (final Exception e) {
+			if(CAUGHT.add(idIn)) {
+				Frex.LOG.info("Unable to load render material " + idIn.toString() + " due to exception " + e.toString());
+			}
+		}
+
+		return result;
+	}
 }
