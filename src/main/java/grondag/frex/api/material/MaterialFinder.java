30462f7b9c4d4740c5ac3ebceb91d02f6908155c
Grondag <grondag@gmail.com>
2020-05-26 06:29:34 -0700

    upfate to 20w21a

--- src/main/java/grondag/frex/api/material/MaterialFinder.java
+++ src/main/java/grondag/frex/api/material/MaterialFinder.java
@@ -31,15 +31,50 @@ import grondag.frex.api.Renderer;
  */
 @API(status = API.Status.STABLE)
 public interface MaterialFinder extends net.fabricmc.fabric.api.renderer.v1.material.MaterialFinder {
-
 	@Override
 	MaterialFinder clear();
 
+	/**
+	 * Values > 1 enable decal rendering. Sprites at index 1+
+	 * are blended with the sprite at index 0.<p>
+	 *
+	 * Lighting, coloring and shading can be controlled for individual
+	 * decal layers but geometry, vertex normals,  blend mode and condition
+	 * will all be shared.<p>
+	 *
+	 * Decals on top of a non-translucent base layer <em>must</em> are not
+	 * sorted from the camera perspective as a performance optimization.
+	 * This means decals on top of cutout textures must not have any
+	 * pixels with alpha > 0 if the base sprite is cutout there.
+	 * Visual errors due to incorect sorting will result. <p>
+	 *
+	 * @param depth 1 up to  {@link Renderer#maxSpriteDepth()}
+	 */
 	@Override
 	MaterialFinder spriteDepth(int depth);
 
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @deprecated
+	 * In FREX the blend mode explicitly applies only to the base
+	 * quad, not any decal layers and Indigo does not support sprite
+	 * depth at all so the spriteIndex parameter is unnecessary.
+	 * Use {@link #blendMode(BlendMode)} instead.
+	 */
+	@Deprecated
 	@Override
 	MaterialFinder blendMode(int spriteIndex, BlendMode blendMode);
+
+	/**
+	 * Defines how sprite pixels will be blended with the scene.
+	 * Affects only the base sprite at index 0 - decal sprites are
+	 * always blended as if translucent.
+	 *
+	 * @param blendMode
+	 * @return
+	 */
+	MaterialFinder blendMode(BlendMode blendMode);
 
 	@Override
 	MaterialFinder disableColorIndex(int spriteIndex, boolean disable);
@@ -53,8 +88,13 @@ public interface MaterialFinder extends net.fabricmc.fabric.api.renderer.v1.mate
 	@Override
 	MaterialFinder emissive(int spriteIndex, boolean isEmissive);
 
-	@API(status = API.Status.EXPERIMENTAL)
+	@Deprecated
+	@API(status = API.Status.DEPRECATED)
 	MaterialFinder shader(MaterialShader pipeline);
+
+	@Deprecated
+	@API(status = API.Status.EXPERIMENTAL)
+	MaterialFinder shader(int spriteIndex, MaterialShader pipeline);
 
 	@API(status = API.Status.EXPERIMENTAL)
 	MaterialFinder condition(MaterialCondition condition);
